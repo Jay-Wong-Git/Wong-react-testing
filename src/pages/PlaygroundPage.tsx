262c9334213af202a6a4b195771d69e2507e0029
= <=>
2024-05-16 22:34:53 +0800

    Testing ToastDemo

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,14 @@
-import SearchBox from "../components/SearchBox";
+import { Toaster } from "react-hot-toast";
+
+import ToastDemo from "../components/ToastDemo";
 
 const PlaygroundPage = () => {
-  return <SearchBox onChange={(searchTerm) => console.log(searchTerm)} />;
+  return (
+    <>
+      <ToastDemo />
+      <Toaster />
+    </>
+  );
 };
 
 export default PlaygroundPage;
